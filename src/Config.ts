5c9b08200fb9774ff2775763875cb279f8b2f109
Lewis Kimani <kimanilewi@gmail.com>
2023-10-02 12:34:33 +0300

    error handling and name change

--- src/Config.ts
+++ src/Config.ts
@@ -1,6 +1,6 @@
 // config.ts
 const config = {
-    API_BASE_URL: 'http://localhost/api/', // Replace with your API base URL
+    API_BASE_URL: 'http://localhost:9001/index.php', // Replace with your API base URL
   };
   
   export default config;
