56a4b35a223af4b6e517882334574967ac054899
Lewis Kimani <kimanilewi@gmail.com>
2023-10-03 12:51:43 +0300

    mpesa api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import config from './config';
+import { Buffer } from 'buffer'
 
 const API_BASE_URL = config.API_BASE_URL;
 
@@ -11,37 +12,89 @@ const App: React.FC = () => {
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
+  // Validation functions (as mentioned earlier)
   const handleMobileNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setMobileNumber(e.target.value); 
   };
-
-  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setAmount(e.target.value);
+  
+  const isValidAmount = (amount: string): boolean => {
+    const amountValue = parseFloat(amount);
+    return !isNaN(amountValue) && amountValue > 0;
   };
-
+  
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (isValidAmount(value)) {
+      setAmount(value);
+    } else {
+      // Show error message or handle invalid input
+    }
+  };
   const handlePayment = async () => {
+    if (!isValidAmount(amount)) {
+      // Handle invalid input, show error message, etc.
+      return;
+    }
+    const generateBase64Password = (shortcode: string, passkey: string, timestamp: string): string => {
+      const combinedString = `${shortcode}${passkey}${timestamp}`;
+      const encodedString = Buffer.from(combinedString).toString('base64');
+      return encodedString;
+    };
     setLoading(true);
     setSuccessMessage('');
     setErrorMessage('');
 
+    // Construct the payload
+    const now = new Date();
+  const year = now.getFullYear().toString();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const date = String(now.getDate()).padStart(2, '0');
+  const hour = String(now.getHours()).padStart(2, '0');
+  const minute = String(now.getMinutes()).padStart(2, '0');
+  const second = String(now.getSeconds()).padStart(2, '0');
+
+  const timestamp = `${year}${month}${date}${hour}${minute}${second}`;
+
+  const password = generateBase64Password("174379", "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919", timestamp);
+    const payload = {
+      BusinessShortCode: 174379,
+      Password : password,
+      Timestamp: timestamp,
+      TransactionType: "CustomerPayBillOnline",
+      Amount: 5,
+      PartyA: mobileNumber,
+      PartyB: 174379,
+      PhoneNumber: mobileNumber,
+      CallBackURL: "https://mydomain.com/path",
+      AccountReference: "SAFARI",
+      TransactionDesc: "Payment for charge",
+    };
+
+    console.log(payload);
     try {
-      const response = await fetch(`${API_BASE_URL}/payment`, {
+      const response = await fetch(`${API_BASE_URL}/processrequest`, 
+      {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          'Authorization': `Bearer Xi385RA3ncqPMzatTYwUGHV7OpZE`, // Add your Bearer token here
         },
-        body: JSON.stringify({
-          mobileNumber,
-          amount,
-        }),
+        body: JSON.stringify(payload),
       });
-
+ 
+      console.log(response);
       if (response.ok) {
-        setSuccessMessage('Payment successful!');
+        const data = await response.json();
+        console.log(data);
+        // Handle success response from the API if needed
+        setSuccessMessage(data.message);
       } else {
+        // Handle error response from the API
         setErrorMessage('Payment failed. Please try again.');
       }
     } catch (error) {
+      console.log(error);
+      // Handle network error
       setErrorMessage('Please complete MPESA payment(KES ' + amount + ') sent on your phone. ' +  mobileNumber);
     } finally {
       setLoading(false);
@@ -50,7 +103,7 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <h1>Safari Payment App</h1>
+      <h1>Safari Power</h1>
       <div>
         <label>Mobile Number:</label>
         <input type="text" value={mobileNumber} onChange={handleMobileNumberChange} />
--- src/Config.ts
+++ src/Config.ts
@@ -1,6 +1,7 @@
 // config.ts
 const config = {
-    API_BASE_URL: 'http://localhost:9001/index.php', // Replace with your API base URL
+    API_BASE_URL: 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1', // Replace with your API base URL
+    PASS: 'WlJC5TADX5opFkp3jxGom4yyquk7x5GCIiCNJTGydXoBbGfZtH3as6ll3pUZR19uOnrcLKCtYYq6RX9PYJ8cLtl2dTbNh+1Lt4WVA8z4kkpAFTdZjE7O7qmvVwWQ2G1kmqyTAPJKTuamifmKL2vGGoyMc1BI4dHe4V/hbgv7Vsgnhy0uCYiCIGp29QGqHQUyTfltzgeG2Dfeu/+AUp5N7o/cZsGoWy+f+NUD60MVxbyO86Pff0DwCtMMnGwTLmE5ElIn8Q2BZKEwH4XJ+Ung7hY/TNbUtH/naugwRHaNnQCA1hEwirYqoeyOGgahfDTR9mWzN7AHD4/HsyoMOAF2bw=='
   };
   
   export default config;
